7e2eccfcfaf6e41d2b3d328cf8c081eb937be194
TranTinh030892 <trantinh.ptit@gmail.com>
2024-03-01 10:41:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/medihealth/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.medihealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private EditText edit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        edit = findViewById(R.id.edit);
+    }
+}
